2553d4ac011d3a4f2b0cc34553ab5f52707a624e
Stanisław Pitucha <stan.pitucha@envato.com>
2019-08-23 10:58:52 +1000

    Split out the private key decryption

--- lib/ejson_wrapper.rb
+++ lib/ejson_wrapper.rb
@@ -6,12 +6,16 @@ require "ejson_wrapper/generate"
 module EJSONWrapper
   def self.decrypt(file_path, key_dir: nil, private_key: nil, use_kms: false, region: nil)
     if use_kms
-      private_key = DecryptPrivateKeyWithKMS.call(file_path, region: region)
+      private_key = private_key_decrypted(file_path, region: region)
     end
     DecryptEJSONFile.call(file_path, key_dir: key_dir, private_key: private_key)
   end
 
   def self.generate(**args)
     Generate.new.call(**args)
+  end
+
+  def self.private_key_decrypted(file_path, region: nil)
+    DecryptPrivateKeyWithKMS.call(file_path, region: region)
   end
 end
